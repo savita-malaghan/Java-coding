9844754d92804aec92e73e55c391e94e7eb2ea23
Savita Malaghan <svtmlghn@gmail.com>
2026-04-28 03:20:06 +0000

    Minimum coins to make targeted amount

---
+++ CoinChange.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class CoinChange {
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println("Minimum coins: " + coinChange(coins, amount));
+    }
+}
